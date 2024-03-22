a20ab4d42a207c2d59bedc1c083d00df7284e129
Arpit <arpit.mishra.out@gmail.com>
2024-03-22 17:48:55 +0530

    Day 3 of 30 days of challenge has been completed

---
+++ day3/js/script.js
@@ -0,0 +1,79 @@
+"use strict";
+
+console.log("30 days of JavaScript");
+// console.log(Boolean(0n));
+
+// Falsey values in JavaScript
+
+// 1 0
+// 2 0n
+// 3 null
+// 4 undefined
+// 5 Nan
+// 6 false
+// '' -> empty string
+
+// alert("30 days of JavaSript");
+// let agree = confirm("Are you sure want to delete this ?");
+// console.log(agree);
+
+const time = new Date();
+// console.log(time);
+
+// gettting full year
+
+// const year = new Date();
+// console.log(year.getFullYear());
+// console.log(year.getMonth() + 1);
+// console.log(year.getDate()); // will get date b/w 1 to 31
+// console.log(year.getDay());
+// console.log(year.getHours());
+// console.log(year.getMinutes());
+
+// Day 3 Exercise
+
+let firstName = "Arpit";
+let lastName = "Mishra";
+let country = "India";
+let city = "Jaipur";
+let isMarried = false;
+let year = time.getFullYear();
+// console.log(typeof firstName, firstName);
+// console.log(typeof lastName, lastName);
+// console.log(typeof country, country);
+// console.log(typeof city, city);
+// console.log(typeof isMarried, isMarried);
+// console.log(typeof year, year);
+// console.log(typeof `10` == typeof 10);
+// console.log(typeof parseInt(`10`) == typeof 10);
+// console.log(Boolean(78));
+// console.log(Boolean("hllo"));
+// console.log(Boolean({}));
+// console.log(Boolean(``));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(4 !== "4");
+
+// Level 2 Exercise
+
+// const base = prompt("Enter base of triangle: ");
+// const height = prompt("Enter height of triangle: ");
+
+// const areaOfTriangle = 0.5 * +base * +height;
+// console.log(`Area of triangle is: ${areaOfTriangle}`);
+
+// const side1 = +prompt("Enter side 1 of triangle: ");
+// const side2 = parseInt(prompt("Enter side 2 of triangle: "));
+// const side3 = Number(prompt("Enter side 3 of triangle: "));
+
+// const perimeterOfTriangle = side1 + side2 + side3;
+// console.log(`Perimeter of triangle: ${perimeterOfTriangle}`);
+
+// const fName = prompt("Enter your name: ");
+// console.log(fName.length > 7 ? `Your name is large: ` : `Your name is short:`);
+
+// const yearsToLive = Number(
+//   prompt("Enter a number of years a person can live: ")
+// );
+// let seconds = yearsToLive * 365 * 24 * 60 * 60;
+// console.log(seconds);
