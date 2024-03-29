f8583c220329d2ab54604cd32423edcaae62e5e2
Arpit <arpit.mishra.out@gmail.com>
2024-03-29 18:32:49 +0530

    Day 5 Of 30 days of js challenge

---
+++ day5/script.js
@@ -0,0 +1,56 @@
+`use strict`;
+
+// Array of Countries
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+
+// Array of webtechs
+
+const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
+
+// // Level 1 Exercise
+
+// // const arr = [];
+// const numArray = [78, 12, 34, 56, 78];
+// console.log(numArray.length);
+
+const itCompanies = [
+  "Facebook",
+  "Google",
+  "Microsoft",
+  "Apple",
+  "IBM",
+  "Oracle",
+  "Amazon",
+];
+
+// const companiesWithMoreo = [];
+
+// for (let i = 0; i < itCompanies.length; i++) {
+//   let firstTime = itCompanies[i].toLowerCase().indexOf("o");
+//   let lastTime = itCompanies[i].toLowerCase().lastIndexOf("o");
+//   if (firstTime > 0 && lastTime > 0 && firstTime !== lastTime) {
+//     companiesWithMoreo.push(itCompanies[i]);
+//   }
+// }
+
+// console.log(companiesWithMoreo);
